c7a990091002cce967e2c51acc79f11b938934e3
chrisjones120 <chris.jones120@outlook.com>
2017-05-01 12:52:29 +1000

    Map working

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 import React from 'react';
 import AppBar from 'material-ui/AppBar';
 import Drawer from 'material-ui/Drawer';
-import MenuItem from 'material-ui/MenuItem';
 import getMuiTheme from 'material-ui/styles/getMuiTheme';
 import lightBaseTheme from 'material-ui/styles/baseThemes/lightBaseTheme';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
-import Draggable from './components/WorldMap.js';
+import Draggable from './components/Draggable.js';
 
 class App extends React.Component {
   constructor(props) {
@@ -18,7 +17,7 @@ class App extends React.Component {
   handleToggle = () => this.setState({open: !this.state.open});
 
   handleClose = () => this.setState({open: false});
-  
+
   move = (e) => this.setState(e);
 
   render() {
@@ -36,11 +35,11 @@ class App extends React.Component {
             open={this.state.open}
             onRequestChange={(open) => this.setState({open})}
           >
-			<h1>WW1 Information</h1>
-			<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc malesuada metus euismod lectus consequat suscipit. Suspendisse in nibh odio. Suspendisse mollis odio at ante tincidunt sodales. Fusce molestie ut diam a consequat. Pellentesque pulvinar tempus sem, ac vestibulum ante varius et. Maecenas ut arcu vitae augue fringilla sagittis nec et tellus. Nulla eu massa pretium, ultrices neque ac, sollicitudin metus.</p>
+			    <h1>WW1 Information</h1>
+			    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc malesuada metus euismod lectus consequat suscipit. Suspendisse in nibh odio. Suspendisse mollis odio at ante tincidunt sodales. Fusce molestie ut diam a consequat. Pellentesque pulvinar tempus sem, ac vestibulum ante varius et. Maecenas ut arcu vitae augue fringilla sagittis nec et tellus. Nulla eu massa pretium, ultrices neque ac, sollicitudin metus.</p>
           </Drawer>
-          <Draggable x={x} y={y} onMove={this.move}>
-            <img src={"map.PNG"} alt={"World Map"} className={"world-map"}/>
+          <Draggable x={x} y={y} onMove={this.move} z={1} width={4500} height={2592}>
+              <img src={"map.PNG"} alt={"World Map"} className={"world-map"}/>
           </Draggable>
         </div>
       </MuiThemeProvider>
--- src/components/Draggable.js
+++ src/components/Draggable.js
@@ -3,13 +3,13 @@ import ReactDOM from 'react-dom';
 
 class Draggable extends React.Component{
   constructor(props) {
-	super(props);
+  	super(props);
     this.state = {relX: 0, relY: 0};
-	this.onMouseDown=this.onMouseDown.bind(this);
-	this.onMouseUp=this.onMouseUp.bind(this);
-	this.onMouseMove=this.onMouseMove.bind(this);
+  	this.onMouseDown=this.onMouseDown.bind(this);
+  	this.onMouseUp=this.onMouseUp.bind(this);
+  	this.onMouseMove=this.onMouseMove.bind(this);
   }
-  
+
   onMouseDown(e) {
     if (e.button !== 0) return;
     const ref = ReactDOM.findDOMNode(this.refs.handle);
@@ -23,37 +23,39 @@ class Draggable extends React.Component{
     document.addEventListener('mouseup', this.onMouseUp);
     e.preventDefault();
   }
-  
+
   onMouseUp(e) {
     document.removeEventListener('mousemove', this.onMouseMove);
     document.removeEventListener('mouseup', this.onMouseUp);
     e.preventDefault();
   }
-  
+
   onMouseMove(e) {
-	const body = document.body;
-	var tempX = e.pageX - this.state.relX;
-	var tempY = e.pageY - this.state.relY;
-	if (tempX <= 0 && tempX + 4500 >= body.clientWidth) {
-		this.props.onMove({x: tempX});
-	} else {
-		this.setState({relX:tempX});
-	}
-	if (tempY <= 0 && tempY + 2592 >= body.clientHeight) {
-		this.props.onMove({y: tempY});
-	} else {
-		this.setState({relY:tempY});
-	}
-	e.preventDefault();
+  	const body = document.body;
+  	var tempX = e.pageX - this.state.relX;
+  	var tempY = e.pageY - this.state.relY;
+  	if (tempX <= 0 && tempX + this.props.width * this.props.z >= body.clientWidth) {
+  		this.props.onMove({x: tempX});
+  	} else {
+  		this.setState({relX:tempX});
+  	}
+  	if (tempY <= 0 && tempY + this.props.height * this.props.z >= body.clientHeight) {
+  		this.props.onMove({y: tempY});
+  	} else {
+  		this.setState({relY:tempY});
+  	}
+  	e.preventDefault();
   }
-  
+
   render() {
     return <div
       onMouseDown={this.onMouseDown}
       style={{
         position: 'absolute',
         left: this.props.x,
-        top: this.props.y
+        top: this.props.y,
+        width: this.props.width * this.props.z,
+        height: this.props.height * this.props.z
       }}
       ref="handle"
     >{this.props.children}</div>;
